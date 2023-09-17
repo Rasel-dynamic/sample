3b6883370e6d06ecad128eab393542da569709d5
Rasel-dynamic <143400994+Rasel-dynamic@users.noreply.github.com>
2023-09-17 17:24:44 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+//  scrollspy project-image
+
+var imgBox = document.querySelectorAll('.resume-section .col img');
+            for(i=0; i<imgBox.length;i++){
+            imgBox[i].addEventListener('mouseenter', function(hover){
+            var imgHeight = this.scrollHeight;
+            var scrollheight = imgHeight - 300;
+            hover.target.style.objectPosition = `0px -${scrollheight}px`;
+             
+            });
+             
+            imgBox[i].addEventListener('mouseleave', function(hover){
+            hover.target.style.objectPosition = '0px 0px';
+             
+             
+            });
+            }
+ 
